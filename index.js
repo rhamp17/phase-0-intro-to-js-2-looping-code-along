1e53688c0f8b8afbe31b42828a27506a3bbb2235
Ryan <rhamp17@gmail.com>
2023-05-10 21:32:30 -0500

    Loop Lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"]
+const event = ["birthday"]
+function writeCards(names, event){
+    let messages = []
+    for (let i = 0; i < names.length; i++){
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return messages;
+}
+function countDown(){
+let number = 11
+while (number > 0){
+    console.log(number = number- 1)
+}
+}
